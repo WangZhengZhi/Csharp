922e561ec45142d37e1ee846b5ee5116b5280b9a
王正直 <wang_zheng_zhi@outlook.com>
2017-04-19 10:57:18 +0800

    《C#从入门到精通》表达式与运算符

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Principal;
+
+namespace ConsoleApplication4
+{
+    //表达式与运算符
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*int i = 927;
+            i = i * i + 112;
+            int j = 12;
+            j = j / 2;
+            Console.WriteLine("i={0},j={1}",i,j);*/
+            /*
+            int i = 927;
+            int j = i;
+            Console.WriteLine(Math.Sin(i*i+j*j));
+            */
+           /* int m1;
+            int m2;
+            m1= 927;
+            m2 = m1 + m1;
+            Console.WriteLine("m1={0},m2={1}",m1,m2);*/
+            /*decimal r1 = (decimal) 1112.82;
+            decimal r2 = (decimal) 9270.81;
+            decimal r3;
+            r3 = r2 - r1;
+            Console.WriteLine("r3={0}",r3);*/
+            /*int ls1=10;
+            int ls2=20;
+            int sum;
+            sum = ls1 * ls2;
+            Console.WriteLine(sum);*/
+            /*int shj1=45;
+            int shj2=5;
+            int ls;
+            ls = shj1 / shj2;
+            Console.WriteLine(ls);*/
+
+            /*int i1;
+            int i2;
+            i1 = 55;
+            i2 = 10;
+            int i3;
+            i3 = i1 % i2;
+            Console.WriteLine(i3);*/
+            /*int i = 927;
+            for (int j = 1; j<= 112; j++)
+            {
+                i++;
+            }
+            Console.WriteLine(i);*/
+           /* int i = 927;
+            i += 112;
+            Console.WriteLine(i);
+            */
+           /* bool result;
+            int i = 1;
+            int j = 1;
+            result = i == j;
+            Console.WriteLine(result);
+            */
+           /* int num1;
+            int num2;
+            num1 = 1;
+            num2 = 1;
+            bool result;//BOOL值
+            result = (num1 & num2)==1;
+            Console.WriteLine(result);
+            result = (num1 | num2) == 1;
+            Console.WriteLine(result);
+            */
+            int num1;
+            int num2;
+            num1 = 98;
+            num2 = 99;
+            int result;
+            result = (num1 ^ num2);
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
