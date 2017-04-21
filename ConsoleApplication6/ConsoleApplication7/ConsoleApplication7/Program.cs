ece6953f4697768769ebef2dbcc7ef0e31aa6a22
王正直 <wang_zheng_zhi@outlook.com>
2017-04-21 20:34:55 +0800

    《C#从入门到精通》复习第五章的内容； 1:DateTime.now 2:Compare(); 3:CompareTo() 4:Equlas()

---
+++ ConsoleApplication6/ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //温习下上次的内容
+           /* DateTime dt = DateTime.Now;
+            Console.WriteLine("{0}",string.Format("{0:F}",dt));
+            string strb = string.Format("{0:f}", dt);
+            Console.WriteLine(strb);
+            strb = string.Format("{0:d}",dt);
+            Console.WriteLine(strb);
+            */
+            //问题1：string 与String的区别
+           /*______________比较字符串*/
+            string str1 = "hello world";
+            string str2 = "hello world";
+            string str3 = "hello csharp";
+            //String.Compare(string1,string2)
+            Console.WriteLine(string.Compare(str1,str2));//str1=str2 value= 0;
+            Console.WriteLine(string.Compare(str1,str3));//str1!=str3 value=1;
+            Console.WriteLine(string.Compare(str3,str1));//str3!=str1  value=-1;
+            //sring.Compare()
+            Console.WriteLine(str1.CompareTo(str2));//str1=str1 value=0
+            Console.WriteLine(str1.CompareTo(str3));//str1!=str3 value=1
+            Console.WriteLine(str3.CompareTo(str1));//str3!=str1 value=-1
+            //string1.Equals(sring2)
+            //Equals(string1,strng2)
+            Console.WriteLine(Equals(str1,str2));//str1=str2 value=True
+            Console.WriteLine(Equals(str1,str3));//str1!=str3 value=False
+            Console.WriteLine(str1.Equals(str2));//str1=str2 value=True
+            Console.WriteLine(str1.Equals(str3));//str1!=str3 value=False
+
+
+
+
+        }
+
+    }
+}
