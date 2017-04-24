cbc6c3f58411bc3a78583c77391c8844d772b4bc
王正直 <wang_zheng_zhi@outlook.com>
2017-04-24 20:39:12 +0800

    1.开发一个程序实现字符串中的每个字符颠倒输出

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.SqlServer.Server;
+
+namespace ConsoleApplication10
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //实践与练习
+            //1.开发一个程序实现字符串中的每个字符颠倒输出
+            string str1 =Console.ReadLine();
+
+
+            for (int i = 1; i<=str1.Length; i++)
+            {
+                Console.Write(str1.Substring(str1.Length-i,1));
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
