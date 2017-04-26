8054a1d440a10a68e1e87785bc445a865628bc47
王正直 <wang_zheng_zhi@outlook.com>
2017-04-26 20:34:14 +0800

    《C#从入门到精通》———去掉字符串中的空格

---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApplication11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //去除字符串中所有的空格
+            /*
+            string str = "hello world";
+            Console.WriteLine(str.Remove(5,1));
+*/
+
+            //-----
+            Console.WriteLine("please input u words");
+            string str = Console.ReadLine();
+            int num;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    Console.WriteLine(str.Remove(i,1));
+                }
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
