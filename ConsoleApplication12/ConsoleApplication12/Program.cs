3a899b782bf4fafcd8994bcda4e4f5de72e476c8
王正直 <wang_zheng_zhi@outlook.com>
2017-04-26 21:14:41 +0800

    《C#从入门到精通》第六章————————if——else语句

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //第六章：流程控制语句
+            //if---else 语句
+           /* int i = 928;
+            if (i > 927)
+            {
+                Console.WriteLine("i大于927");
+            }
+            else
+            {
+                Console.WriteLine("i小于927");
+
+            }*/
+            //
+            /*
+            const int i = 18;
+            const int j = 30;
+            const int k = 50;
+            int uage=0;
+            Console.WriteLine("please input u age");
+            uage=int.Parse(Console.ReadLine());
+            if (uage <= i)
+            {
+                Console.WriteLine("u age is under 18");
+
+            }
+            else
+            {
+
+                if (uage >= 18 && uage <= 30)
+                {
+                    Console.WriteLine("under 30 over 18");
+                }
+                if (uage > 30 && uage <= 50)
+                {
+                    Console.WriteLine("under 50 over 30");
+                }
+
+
+            }
+            */
+            
+
+
+
+
+
+
+
+        }
+    }
+}
