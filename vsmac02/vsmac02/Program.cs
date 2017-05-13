0d704b9059807d4c089cf1c11bd7f1350c358aa6
王正直 <wang_zheng_zhi@outlook.com>
2017-05-13 21:55:50 +0800

    《C#从入门到精通》第十章接口的实现与继承—visual studio for Mac

---
+++ vsmac02/vsmac02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace vsmac02
+{
+    interface Myinterface
+    {
+        string ID { get; set; }
+        string NAME { get; set; }
+        void ShowInfo();
+        {
+            Console.WriteLine("编号\t+姓名");
+            Console.WriteLine(ID+"\t"+NAME);
+
+        }
+
+
+
+}
+
+class Program : Myinterface
+{
+    string id = "";
+    string name = "";
+    public string ID { get { return id; } set { id = value; } }
+    public string NAME { get { return name; } set { name = value; } }
+    public void ShowInfo()
+    {
+        Console.WriteLine("编号\t+姓名");
+        Console.WriteLine(ID + "\t" + NAME);
+    }
+
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        Myinterface myinterface = program;
+
+
+    }
+
+}
---
+++ vsmac03/vsmac03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace vsmac03
+{
+    interface People
+    {
+        string Name { get; set; }
+        string Sex { get; set; }
+
+    }
+    interface Teacher : People
+    {
+        void teach();
+    }
+    interface Student : People
+    {
+        void study();
+    }
+
+
+
+    class Program : People, Teacher, Student
+    {
+        string name = "";
+        string sex = "";
+        public string Name { get { return name; } set { name = value; } }
+        public string Sex { get { return sex; } set { sex = value; } }
+        public void teach() { Console.Write(Name + "  " + Sex + "教师"); }
+        public void study() { Console.WriteLine(Name + " " + Sex + "学生"); }
+
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Teacher teacher = program;
+            teacher.Name = "wang bo";
+            teacher.Sex = "BOY";
+            teacher.teach();
+            Student student = program;
+            student.Name = "wang zhengzhi";
+            student.Sex = "BOY";
+            student.study();
+
+        }
+    }
+}
