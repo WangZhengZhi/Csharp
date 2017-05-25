d02ef0f0877215d0bb8e5f196d58debd1d7bb6f4
王正直 <wang_zheng_zhi@outlook.com>
2017-05-25 21:54:28 +0800

    《C#从入门到精通》第十章接口问题

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//接口问题-第十章
+namespace ConsoleApp1
+{
+    interface Imyinterface
+    {
+        string ID { get; set; }
+        string NAME { get; set; }
+        void ShowInfo();
+        
+
+
+    }
+    class Program:Imyinterface
+    {
+        string id="";
+        string name="";
+        public string ID { get { return id; }set { id = value; } }
+        public string NAME { get { return name; }set { name = value; } }
+        public void ShowInfo()
+        {
+            Console.WriteLine("id \t name");
+            Console.WriteLine(ID+"\t"+NAME);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Imyinterface imyinterface = program;
+            imyinterface.ID = "15426037";
+            imyinterface.NAME = "zhengzhi";
+            imyinterface.ShowInfo();
+
+
+
+        }
+    }
+   
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program:Ipeople,Istudent,Iteach
+    {
+        string name;
+        string sex;
+        public string NAME { get { return name; }set { name = value; } }
+        public string SEX { get { return sex; }set { sex = value; } }
+        public void teach()
+        {
+            Console.WriteLine(NAME + " " + SEX + " " + "教师");
+        }
+        public void study()
+        {
+            Console.WriteLine(NAME + " " + SEX + " " + "学生");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Iteach iteach = program;
+            iteach.NAME = "zhengzhi";
+            iteach.SEX = "boy";
+            iteach.teach();
+            Istudent istudent = program;
+            istudent.NAME = "wangbo";
+            istudent.SEX = "boy";
+            istudent.study();
+
+        }
+    }
+    interface Ipeople
+    {
+        string NAME { get; set; }
+        string SEX { get; set; }
+
+    }
+    interface Istudent : Ipeople
+    {
+        void study();
+
+    }
+    interface Iteach : Ipeople
+    {
+        void teach();
+    }
+
+
+
+
+}
