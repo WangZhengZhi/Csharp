740994d2e03f1c933b461ac120ee8dcbb24a507f
正直 王 <wang_zheng_zhi@outlook.com>
2018-09-21 12:31:52 +0800

    这是我再次深入了解C#从最基本的内容理解

---
+++ ConsoleApp4_class4/Recovered-9月-20-2018-1001上午。Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ConsoleApp4_class4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*对象实例化
+             * 
+             * Form form = new Form();
+            form.Text = "My Dialog";
+            form.ShowDialog();
+            
+             */  
+             
+
+        }
+    }
+}
